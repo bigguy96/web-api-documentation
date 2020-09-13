0fdbb475a1cec167ff1fa87aa7553a01f347607b
GT <gaetan23@msn.com>
2020-09-12 22:46:34 -0400

    added assets and side menu display.

--- WebApiDocumentationWebApplication/Controllers/HomeController.cs
+++ WebApiDocumentationWebApplication/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using ConsoleApp;
+using ConsoleApp.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using WebApiDocumentationWebApplication.Models;
@@ -24,8 +25,28 @@ namespace WebApiDocumentationWebApplication.Controllers
             var openApiDocument = await OpenApiDocumentHelper.CreateAsync();
             var openApiDocumentDetails = new OpenApiDocumentDetails(openApiDocument);
             var pathGroupings = openApiDocumentDetails.Paths.Select(path => path.Operations.GroupBy(operation => operation.Name));
-            var vm = new HomeViewModel 
-            { 
+
+            ////var z = openApiDocumentDetails.Paths.ToDictionary(x => x.Operations.GroupBy(o => o.Name), x => x.Operations);
+            //var g = openApiDocument.Paths.ToDictionary(x => x.Key, x => x.Value);
+            //var r = openApiDocument.Paths.Select(g => g.Value.Operations.GroupBy(s =>  s.Value.Tags[0].Name));
+            //var previous = string.Empty;
+            //var current = string.Empty;
+            //var d = new Dictionary<string, IEnumerable<Operation>>();
+            //foreach (var item in openApiDocument.Paths)
+            //{
+            //    foreach (var o in item.Value.Operations)
+            //    {
+            //        //current = o.Name;
+
+            //        if (!current.Equals(previous))
+            //        {
+            //        }
+            //    }
+            //    previous = current;
+            //}
+
+            var vm = new HomeViewModel
+            {
                 Paths = openApiDocumentDetails.Paths,
                 Groupings = pathGroupings,
                 Components = openApiDocumentDetails.Components,
