007e085063a2c61b4ab98680dbaa7d3173fb92f1
GT <gaetan23@msn.com>
2020-09-13 20:27:47 -0400

    removed unused files.

--- WebApiDocumentationWebApplication/Controllers/HomeController.cs
+++ WebApiDocumentationWebApplication/Controllers/HomeController.cs
@@ -33,17 +33,6 @@ namespace WebApiDocumentationWebApplication.Controllers
             };
 
             return View(vm);
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        }        
     }
 }
