8b639bfc1ca1348fd9e2171c8edec0cb5fe39582
GT <gaetan23@msn.com>
2020-09-19 20:19:07 -0400

    Added report views.

---
+++ WebApiDocumentationWebApplication/Models/ReportsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.OpenApi.Models;
+using System.Collections.Generic;
+using WebApiDocumentationLibrary.Entities;
+
+namespace WebApiDocumentationWebApplication.Models
+{
+    public class ReportsViewModel
+    {
+        public IEnumerable<Operation> Operations { get; set; }
+        public string WebApiTitle { get; internal set; }
+        public string WebApiUrl { get; internal set; }
+        public OpenApiComponents Components { get; internal set; }
+    }
+}
