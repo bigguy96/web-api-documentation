a083a01a11fbfc4c700f6b3022f31f3d2a78c886
GT <gaetan23@msn.com>
2020-09-20 13:56:42 -0400

    Display schemas.

---
+++ WebApiDocumentationWebApplication/Models/SchemasViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using WebApiDocumentationLibrary.Entities;
+
+namespace WebApiDocumentationWebApplication.Models
+{
+    public class SchemasViewModel
+    {
+        public string WebApiTitle { get; internal set; }
+        public string WebApiUrl { get; internal set; }
+        public IEnumerable<Schema> Schemas { get; internal set; }
+    }
+}
