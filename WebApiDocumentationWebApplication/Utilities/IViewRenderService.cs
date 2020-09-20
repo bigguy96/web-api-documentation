dc056f8c879f034803d7c6dbca68e2409dd6d8ad
GT <gaetan23@msn.com>
2020-09-20 11:14:14 -0400

    Added link to page.

--- WebApiDocumentationWebApplication/Utilities/IViewRenderService.cs
+++ WebApiDocumentationWebApplication/Utilities/IViewRenderService.cs
@@ -9,3 +9,4 @@ namespace WebApiDocumentationWebApplication.Utilities
 }
 //https://ppolyzos.com/2016/09/09/asp-net-core-render-view-to-string/
 //https://www.learnrazorpages.com/advanced/render-partial-to-string
+//https://gist.github.com/zckkte/66b04a18519284ebe25e83391cc9913b
