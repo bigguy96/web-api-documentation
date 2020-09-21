3d61a26b9a970ead0bea515c52f12b5a7ea6e1cb
GT <gaetantauvette@gmail.com>
2020-09-21 08:34:50 -0400

    Added search api page.

--- WebApiDocumentationWebApplication/Controllers/OperationsController.cs
+++ WebApiDocumentationWebApplication/Controllers/OperationsController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebApiDocumentationWebApplication.Models;
@@ -17,7 +18,7 @@ namespace WebApiDocumentationWebApplication.Controllers
             _emailService = emailService;
         }
 
-        public async Task<IActionResult> IndexAsync(string operation)
+        public async Task<IActionResult> Index(string operation)
         {
             var openApiDocumentDetails = await GetApiDocumentDetailsAsync();
             var operations = openApiDocumentDetails.Paths.SelectMany(path => path.Operations.Where(o => o.Name.Equals(operation)));
@@ -28,12 +29,41 @@ namespace WebApiDocumentationWebApplication.Controllers
                 WebApiTitle = openApiDocumentDetails.WebApiTitle,
                 WebApiUrl = openApiDocumentDetails.WebApiUrl
             };
-            var result = await _viewRenderService.RenderToStringAsync("Operations/Index", vm);
-            await _emailService.SendAsync("noreply@fakemail.com", "test", "test", result);
-
+            
+            //var result = await _viewRenderService.RenderToStringAsync("Operations/Index", vm);
+            //await _emailService.SendAsync("noreply@fakemail.com", "test", "test", result);
             //var myDocuments = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
 
             return View(vm);
+        }
+
+        public async Task<IActionResult> Result(string endpoint)
+        {
+            var openApiDocumentDetails = await GetApiDocumentDetailsAsync();
+            var operations = openApiDocumentDetails.Paths.SelectMany(path => path.Operations.Where(o => o.Endpoint.Equals(endpoint, StringComparison.OrdinalIgnoreCase)));
+            var vm = new OperationsViewModel
+            {
+                Operations = operations,
+                Components = openApiDocumentDetails.Components,
+                WebApiTitle = openApiDocumentDetails.WebApiTitle,
+                WebApiUrl = openApiDocumentDetails.WebApiUrl
+            };
+
+            return View(vm);
+        }
+
+        public async Task<IActionResult> Search()
+        {
+            var openApiDocumentDetails = await GetApiDocumentDetailsAsync();
+            var vm = new SearchViewModel { WebApiTitle = openApiDocumentDetails.WebApiTitle, WebApiUrl = openApiDocumentDetails.WebApiUrl };
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public IActionResult Search(SearchViewModel searchViewModel)
+        {
+            return RedirectToAction("Result", new {endpoint = searchViewModel.Endpoint});
         }
     }
 }
---
+++ WebApiDocumentationWebApplication/Models/SearchViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiDocumentationWebApplication.Models
+{
+    public class SearchViewModel
+    {
+        public string Endpoint { get; set; }
+        public string WebApiTitle { get; set; }
+        public string WebApiUrl { get; set; }
+    }
+}
